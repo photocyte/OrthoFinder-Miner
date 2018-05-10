5302a8fd5435ea7d6d249ec7431e8c0104d8cc89
Timothy Fallon <tfallon@mit.edu>
2018-05-10 15:00:48 -0400

    argparse added

--- orthofinder-miner.py
+++ orthofinder-miner.py
@@ -1,1 +1,9 @@
 import Bio
+import argparse
+
+parser = argparse.ArgumentParser(description='Proof of concept script for comparative genomics with OrthoFinder')
+parser.add_argument('--orthofinder_output', required=True, nargs=1,metavar="(FILE)",help="OrthoFinder orthogroups.csv file")
+
+args = parser.parse_args()
+
+
